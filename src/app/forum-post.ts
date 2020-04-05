e53ae41f3d8cdf2e73f51b28b0c5ae4cb0617154
Matt Clark <clark.matthewr@gmail.com>
2020-04-04 19:04:04 -0500

    change name of ForumSubPost type to ForumReply

--- src/app/forum-post.ts
+++ src/app/forum-post.ts
@@ -1,9 +1,9 @@
-import { ForumSubPost } from './forum-sub-post';
+import { ForumReply } from './forum-reply';
 
 export interface ForumPost {
   title: string,
   author: string,
   time: string,
   message: string,
-  replies: ForumSubPost[],
+  replies: ForumReply[],
 }
--- src/app/forum-reply.ts
+++ src/app/forum-reply.ts
@@ -1,4 +1,4 @@
-export interface ForumSubPost {
+export interface ForumReply {
   author: string,
   time: string,
   message: string,
