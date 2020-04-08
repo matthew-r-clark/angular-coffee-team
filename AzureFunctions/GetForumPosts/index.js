0a9b09abcac27322c0cae3b6e519384e59567380
Matt Clark <clark.matthewr@gmail.com>
2020-04-08 13:31:09 -0500

    initial commit for new function

--- AzureFunctions/GetForumPosts/index.js
+++ AzureFunctions/GetForumPosts/index.js
@@ -1,13 +1,74 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = function (context, req) {
+  const { Connection, Request } = require("tedious");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const config = {
+    authentication: {
+      options: {
+        userName: process.env["DB_USERNAME"],
+        password: process.env["DB_PASSWORD"],
+      },
+      type: "default",
+    },
+    server: "coffee-team-sql-server.database.windows.net",
+    options: {
+      database: "coffee-team-db",
+      encrypt: true,
+    },
+  };
 
-    context.res = {
+  const connection = new Connection(config);
+  connection.on("connect", err => {
+    if (err) {
+      context.log(err.message);
+    } else {
+      queryDatabase();
+    }
+  });
+
+  function queryDatabase() {
+    let result = {};
+
+    const query = new Request(
+      'SELECT * FROM test;',
+      (err, rowCount) => {
+        if (err) {
+          console.error("ERROR: " + err.message);
+        } else {
+          console.log(rowCount + ' row(s) returned.');
+        }
+
+        connection.close();
+      }
+    );
+
+    query.on("row", columns => {
+      let id;
+      
+      columns.forEach(column => {
+        console.log(`${column.metadata.colName}: ${column.value}`);
+        let colName = column.metadata.colName;
+        let value = column.value;
+        if (colName === "id") {
+          id = value;
+          result[id] = {}
+        } else {
+          result[id][colName] = value;
+        }
+      });
+    });
+
+    query.on("requestCompleted", () => {
+      context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        body: JSON.stringify(result),
+        headers: {
+          'Access-Control-Allow-Origin': 'http://localhost:4200',
+        },
+      };
+
+      context.done();
+    });
+
+    connection.execSql(query);
+  }
 }
