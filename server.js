b9992d78cc59dec882d211f8265462915d00ebfb
Matt Clark <clark.matthewr@gmail.com>
2020-04-10 11:22:52 -0500

    update to deploy to heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/coffee-team'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/coffee-team/index.html'));
 });
 
