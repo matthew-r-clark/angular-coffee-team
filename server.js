dca8a887b1fcad94cb6c698606bd4ac03ddaa4ff
Matt Clark <clark.matthewr@gmail.com>
2020-04-10 15:14:33 -0500

    remove test route

--- server.js
+++ server.js
@@ -5,16 +5,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/coffee-team'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/coffee-team/index.html'));
-});
-
-app.get('/data', (req, res) => {
-  let data = {
-    firstName: "Matthew",
-    lastName: "Clark",
-  };
-  res.render(data);
 });
 
 app.listen(process.env.PORT || 8080);
