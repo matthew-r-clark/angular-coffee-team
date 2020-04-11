4c4e101aa1f422adcd614a5d269e38233cb56d5a
Matt Clark <clark.matthewr@gmail.com>
2020-04-11 12:49:11 -0500

    apiUrl dynamically set for dev/prod mode

--- src/app/forum.service.ts
+++ src/app/forum.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ForumPost } from './forum-post';
 import { HttpClient } from '@angular/common/http';
 
+let apiUrl: string;
+if (isDevMode()) {
+  apiUrl = "http://localhost:8080/api/GetForumPosts";
+} else {
+  apiUrl = "http://express-coffee-team-server.herokuapp.com/api/GetForumPosts";
+}
+
 @Injectable({
   providedIn: 'root'
 })
-export class ForumService {
-  private url: string = "http://localhost:8080/api/GetForumPosts";
+export class ForumService {  
   public currentTopic = new BehaviorSubject<string>('General');
   public posts: ForumPost[];
 
@@ -18,11 +24,11 @@ export class ForumService {
   }
 
   public getPosts(topic: string) {
-    return this.http.get(`${this.url}/${encodeURIComponent(topic)}`);
+    return this.http.get(`${apiUrl}/${encodeURIComponent(topic)}`);
   }
 
   public getAllPosts() {
-    return this.http.get(this.url);
+    return this.http.get(apiUrl);
   }
 
   public changeTopic(topic: string) {
