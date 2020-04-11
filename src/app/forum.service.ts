3a56cd714327fa15bec9e29cef616b167605d19e
Matt Clark <clark.matthewr@gmail.com>
2020-04-11 10:17:37 -0500

    initialized new api for express server

--- src/app/forum.service.ts
+++ src/app/forum.service.ts
@@ -7,21 +7,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ForumService {
-  // private data = {
-  //   General: [],
-  //   Recipes: [],
-  //   'fav brew method?': [
-  //     { title: 'aeropress', author: 'mattata', time: '6:04pm 03/30/2020', message: 'the most versatile method i know of...', replies: [
-  //       { author: 'jarambe', time: '6:13pm 03/30/2020', message: 'Yeah it really is!'},
-  //       { author: 'mattata', time: '6:42pm 03/30/2020', message: 'what is your favorite recipe?'},
-  //     ] },
-  //     { title: 'Kalita', author: 'jarambe', time: '8:27am 04/01/2020', message: 'My favorite method now days is the Kalita. Love how consistent it is.'},
-  //   ],
-  //   'v60 vs aeropress': [],
-  //   'what does everyone think of James Hoffman?': [],
-  // };
-
-  private url: string = "";
+  private url: string = "http://localhost:8080/api/GetForumPosts";
   public currentTopic = new BehaviorSubject<string>('General');
   public posts: ForumPost[];
 
@@ -32,7 +18,7 @@ export class ForumService {
   }
 
   public getPosts(topic: string) {
-    return this.http.get(`${this.url}?topic=${topic}`);
+    return this.http.get(`${this.url}/${encodeURIComponent(topic)}`);
   }
 
   public getAllPosts() {
@@ -42,8 +28,4 @@ export class ForumService {
   public changeTopic(topic: string) {
     this.currentTopic.next(topic);
   }
-
-  // public getTopics() {
-  //   return Object.keys(this.getAllPosts());
-  // }
 }
