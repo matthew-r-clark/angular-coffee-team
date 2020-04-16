08a8e8f7f993c9aad145cbfed44d863877e195bb
Matt Clark <clark.matthewr@gmail.com>
2020-04-16 16:01:24 -0500

    change forum view

--- src/app/charts-page/charts-page.component.ts
+++ src/app/charts-page/charts-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charts-page.component.css']
 })
 export class ChartsPageComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
